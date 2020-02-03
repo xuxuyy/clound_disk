92e2cd00e14accbe08e5d990407353205c166cf3
xzy <13596217285@163.com>
2020-02-03 12:30:20 +0800

    新文件：   cloud-disk-app/.gitignore 	新文件：   cloud-disk-app/README.md 	新文件：   cloud-disk-app/package-lock.json 	新文件：   cloud-disk-app/package.json 	新文件：   cloud-disk-app/public/favicon.ico 	新文件：   cloud-disk-app/public/index.html 	新文件：   cloud-disk-app/public/manifest.json 	新文件：   cloud-disk-app/src/App.css 	新文件：   cloud-disk-app/src/App.js 	新文件：   cloud-disk-app/src/App.test.js 	新文件：   cloud-disk-app/src/Component/LoginForm.css 	新文件：   cloud-disk-app/src/Component/LoginForm.js 	新文件：   cloud-disk-app/src/Component/MainPanel.css 	新文件：   cloud-disk-app/src/Component/MainPanel.js 	新文件：   cloud-disk-app/src/Logic/Api.js 	新文件：   cloud-disk-app/src/index.css 	新文件：   cloud-disk-app/src/index.js 	新文件：   cloud-disk-app/src/logo.svg 	新文件：   cloud-disk-app/src/serviceWorker.js 	新文件：   cloud-disk-backend/.gitignore 	新文件：   cloud-disk-backend/app.py 	新文件：   cloud-disk-backend/config.py 	新文件：   cloud-disk-backend/model.py 	新文件：   cloud-disk-backend/test.py

---
+++ cloud-disk-backend/model.py
@@ -0,0 +1,31 @@
+from peewee import *
+
+db = SqliteDatabase('mydb.db', pragmas=(('foreign_keys', 'on'),))
+
+
+class BaseModel(Model):
+
+    class Meta:
+        database = db
+
+
+class Folder(BaseModel):
+    name = CharField(max_length=64, unique=True)
+
+
+class File(BaseModel):
+    folder = ForeignKeyField(Folder, backref='files')
+    filename = CharField(index=True)
+    public_share_url = CharField()
+    private_share_url = CharField()
+    private_share_password = CharField()
+    open_public_share = BooleanField()
+    open_private_share = BooleanField()
+
+
+def create_all_tables():
+    db.connect()
+    db.create_tables([Folder, File])
+
+if __name__ == '__main__':
+    create_all_tables()
